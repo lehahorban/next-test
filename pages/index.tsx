b81e96623e6843fa051ed89d4ccdce2085df0570
Oleksii Horban <lehagorban93@gmail.com>
2023-05-26 17:17:59 +0300

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,10 +6,11 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <p>Як справи</p>
-      <h1>Гілка маін</h1>
-      <h1>Hello, World!</h1>
-      <h2>Привіт</h2>
+      <ul>
+        <li>Привіт</li>
+        <li>Привіт</li>
+        <li>Привіт</li>
+      </ul>
     </>
   );
 }
