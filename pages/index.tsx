4c07e7121f23bfd7ed6bfd32c85d5049270fb342
Oleksii Horban <94637142+lehahorban@users.noreply.github.com>
2023-05-26 18:47:21 +0300

    Update index.tsx
    fgfgfgf

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <h1>не правильний код</h1>
+      <h1 className="ssdsds">не правильний код</h1>
       <ul className="hello-name-kjkjkjk">
         <li>Привіт</li>
         <li>Привіт</li>
